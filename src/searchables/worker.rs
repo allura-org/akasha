80f13ed1e9a625d95ce21f9ab58071ad66b53a4a
inflatebot <alfanumeric@protonmail.com>
2026-07-23 18:55:20 -0400

    worker: include elapsed time and jobs/s in the processed-jobs log line

--- src/searchables/worker.rs
+++ src/searchables/worker.rs
@@ -63,9 +63,18 @@ impl SearchWorker {
             // Claiming once per 5s tick would leave a gap of up to the tick
             // period whenever a group finishes faster than 5s.
             loop {
+                let started = std::time::Instant::now();
                 match self.tick().await {
                     Ok(0) => break,
-                    Ok(n) => tracing::info!("SearchWorker processed {} jobs", n),
+                    Ok(n) => {
+                        let secs = started.elapsed().as_secs_f64();
+                        tracing::info!(
+                            "SearchWorker processed {} jobs in {:.2}s ({:.2} jobs/s)",
+                            n,
+                            secs,
+                            n as f64 / secs
+                        );
+                    }
                     Err(e) => {
                         tracing::warn!("SearchWorker error: {e}");
                         break;
